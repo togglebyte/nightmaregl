bcd462733b9c031836454a1017079987d9d50382
Togglebit <me@togglebit.io>
2021-04-14 09:47:46 +0200

    cleanup of text example

--- examples/text.rs
+++ examples/text.rs
@@ -1,32 +1,34 @@
-use nightmaregl::events::{Event, Key, KeyState, LoopAction};
-use nightmaregl::texture::{Texture, Wrap};
-use nightmaregl::{
-    Animation, Color, Context, Position, Renderer, Result, Rotation, Size, Sprite, Viewport,
-};
+use nightmaregl::events::{Event, LoopAction};
 use nightmaregl::text::{Text, WordWrap};
+use nightmaregl::{Color, Context, Position, Renderer, Result, Viewport};
 
 fn main() -> Result<()> {
     let (eventloop, mut context) = Context::builder("Best game ever!").build()?;
 
     let window_size = context.window_size();
-    let mut viewport = Viewport::new(Position::zero(), window_size);
-    let mut renderer = Renderer::default_font(&mut context)?;
+    let viewport = Viewport::new(Position::zero(), window_size);
+    let renderer = Renderer::default_font(&mut context)?;
 
     let font_size = 72.0;
-    let mut text = Text::from_path("/usr/share/fonts/TTF/Hack-Regular.ttf", font_size, WordWrap::NoWrap, &context)?;
+    let mut text = Text::from_path(
+        "/usr/share/fonts/TTF/Hack-Regular.ttf",
+        font_size,
+        WordWrap::NoWrap,
+        &context,
+    )?;
     text.position(viewport.centre().cast());
-    text.set_text("Hello");
+    text.set_text("Hello")?;
 
     eventloop.run(move |event| {
         match event {
-            Event::Draw(dt) => {
+            Event::Draw(_dt) => {
                 context.clear(Color::black());
 
-                renderer.render(
-                	&text.texture(),
-                	&text.vertex_data(),
-                	&viewport,
-                	&mut context
+                let _ = renderer.render(
+                    &text.texture(),
+                    &text.vertex_data(),
+                    &viewport,
+                    &mut context,
                 );
 
                 context.swap_buffers();
